bea3958e52dae379c0a49b9d4d429ab65b62f6ab
faqih28alam <faqih28alam@gmail.com>
2026-02-22 13:46:08 +0700

    add: origin frontend CORS

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,7 +1,7 @@
 import cors, { CorsOptions } from "cors";
 
 const corsOptions: CorsOptions = {
-  origin: "http://localhost:4000",
+  origin: ["http://localhost:4000", "https://link-hub-card.vercel.app"],
   credentials: true,
   optionsSuccessStatus: 200,
 };
